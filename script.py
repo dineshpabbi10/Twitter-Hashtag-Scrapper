f00ffbe8c492168ebc7e4f8a998a28af0466c4f4
dineshpabbi10 <dineshpabbi10@gmail.com>
2019-02-14 21:19:11 +0530

    Add files via upload

--- script.py
+++ script.py
@@ -12,15 +12,20 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
 ### Creating Pandas DataFrame to store Tweets
-t = pd.DataFrame(columns=["Tweets"])
+t = pd.read_csv(r"G:\sss.csv")
+t = pd.DataFrame(t["Tweets"],columns = ["Tweets"])
+print(t)
 
-for tweet in tweepy.Cursor(api.search,q="#trump",count=100,lang="en",since="2018-04-03").items():
+i = 0
+for tweet in tweepy.Cursor(api.search,q="#Kashmir",count=100,lang="en",since="2018-04-03").items():
 	if(tweet.text[:2] == "RT"):
 		pass
 	else:
 		t = t.append({"Tweets":tweet.text.replace("\n"," ")},ignore_index=True)
-	 
-	if len(t)==50:
+		i+=1
+	## Change this condition to store number of tweets
+	
+	if i==50:
 		break
 
 
